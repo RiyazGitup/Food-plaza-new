e2c6e046c34a389c491e947c6731b813d7497975
RiyazGitup <126855829+RiyazGitup@users.noreply.github.com>
2023-04-05 23:50:48 -0700

    Add files via upload

---
+++ Scriptfooditem.js
@@ -0,0 +1,290 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biriyani',
+    rating : 4.3,
+    price: 13,
+    img: '/assets/media/am new.webp',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biriyani',
+    category : 'biriyani',
+    rating : 4.3,
+    price: 15,
+    img: '/assets/media/HydbdChickBiryani.png',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biriyani',
+    category : 'biriyani',
+    rating : 4.3,
+    price: 18,
+    img: '/assets/media/Egg_Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Fish Biriyani',
+    category : 'biriyani',
+    rating : 4.3,
+    price: 15,
+    img: '/assets/media/fish new.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'prawn Biriyani',
+    category : 'biriyani',
+    rating : 4.3,
+    price: 10,
+    img: '/assets/media/Prawns_Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Mandhi Biryani',
+    category : 'biriyani',
+    rating : 4.3,
+    price: 12,
+    img: '/assets/media/mandhi_biriyani.jpg',
+    quantity: 1
+},
+
+{
+    id:7,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: '/assets/media/Chicken_roast.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Chicken Lolypop',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: '/assets/media/chicken_lolypop.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Chicken popcorn',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: '/assets/media/pop new.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Tandoori',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: '/assets/media/Chicken_Tandoori.webp',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Fried Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: '/assets/media/fried chick new.avif',
+    quantity: 1
+},
+
+{
+    id: 12,
+    name: 'Paneer Tikka',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: '/assets/media/paneer-tikka.webp',
+    quantity: 1
+
+},
+{
+    id: 13,
+    name: 'Butter Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: '/assets/media/Butter-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Cheesy Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: '/assets/media/Cheesy paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 15,
+    name: 'Chilli paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 12,
+    img: '/assets/media/ch paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 16,
+    name: 'Paneer-Buttermasala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 8,
+    img: '/assets/media/Paneer-buttermasala.jpg',
+    quantity: 1
+},
+
+{
+    id: 17,
+    name: 'Masala Dosa',
+    category : 'SouthIndian',
+    rating : 4.3,
+    price: 8,
+    img: '/assets/media/mas dosa.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Idly',
+    category : 'SouthIndian',
+    rating : 4.3,
+    price: 7,
+    img: '/assets/media/idly new.jpg',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'Onion uthappam',
+    category : 'SouthIndian',
+    rating : 4.3,
+    price: 5,
+    img: '/assets/media/Onion_uthappam.jpg',
+    quantity: 1
+},
+{
+    id: 20,
+    name: 'Sambar Vada',
+    category : 'SouthIndian',
+    rating : 4.3,
+    price: 7,
+    img: '/assets/media/sambarvada.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Podi Dosa',
+    category : 'SouthIndian',
+    rating : 4.3,
+    price: 10,
+    img: '/assets/media/podi dosa new.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Chicken Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 4,
+    img: '/assets/media/chicken-friedrice.png',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Kim Chi',
+    category : 'chinese',
+    rating : 4.3,
+    price: 4,
+    img: '/assets/media/Kimchi.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Chicken Noodle',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: '/assets/media/Chicken_noodle.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Raman Noodle',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: '/assets/media/raman_noodle.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Bao Bun',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: '/assets/media/Baobun.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Steam Momo',
+    category : 'momo',
+    rating : 4.3,
+    price: 16,
+    img: '/assets/media/steam momo.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Roasted Momo',
+    category : 'momo',
+    rating : 4.3,
+    price: 14,
+    img: '/assets/media/Roasted-Momo.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Green Momo',
+    category : 'momo',
+    rating : 4.3,
+    price: 10,
+    img: '/assets/media/green momo.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'momo',
+    rating : 4.3,
+    price: 8,
+    img: '/assets/media/Momo.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Momo-Nepal',
+    category : 'momo',
+    rating : 4.3,
+    price: 18,
+    img: '/assets/media/Momo_nepal.jpg',
+    quantity: 1
+},
+
+]
+
+export {foodItem};
